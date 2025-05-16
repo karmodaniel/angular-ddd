df76be9246ea4b41900700f9e412c8caecbf35fe
daniel.carmo <daniel.carmo2008@gmail.com>
2025-05-16 00:15:58 -0400

    feat: add new configurations

---
+++ src/app/shared/util/string.util..ts
@@ -0,0 +1,13 @@
+export declare class StringUtil {
+  static readonly EMPTY = '';
+  static readonly HYPHEN = '-';
+  static readonly ASTERISK = '*';
+  static readonly PLUS = '+';
+  static readonly SPACE = ' ';
+  static readonly VERTICAL_BAR = '|';
+  static readonly LEFT_BAR = '\\';
+  static readonly RIGHT_BAR = '/';
+  static readonly COMMA = ',';
+  static readonly POINT = '.';
+  static readonly QUESTION = '?';
+}
